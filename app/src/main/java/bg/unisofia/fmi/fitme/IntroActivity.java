b3fd4727fdcc882f681b84a82223c9138a06a726
Nikolay Mateev <nikolay.mateev@sap.com>
2018-07-01 01:33:30 +0300

    Add initializaiton logic

--- app/src/main/java/bg/unisofia/fmi/fitme/IntroActivity.java
+++ app/src/main/java/bg/unisofia/fmi/fitme/IntroActivity.java
@@ -8,7 +8,7 @@ import android.view.WindowManager;
 
 public class IntroActivity extends AppCompatActivity {
 
-    private static int INTRO_SPLASH_TIME_OUT = 100;
+    private static int INTRO_SPLASH_TIME_OUT = 400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/bg/unisofia/fmi/fitme/MainActivity.java
+++ app/src/main/java/bg/unisofia/fmi/fitme/MainActivity.java
@@ -1,12 +1,17 @@
 package bg.unisofia.fmi.fitme;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.time.DayOfWeek;
 import java.time.temporal.WeekFields;
@@ -17,6 +22,12 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int dailyCalorieGoal;
+    private static int weeklyCalorieGoal;
+
+    private static int[] weeklyCalories = new int[7];
+    private static int[] weeklyWeight = new int[7];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +35,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Current week setup:
         TextView currentWeekText = (TextView) findViewById(R.id.weekText);
 
         String weekStartDate = getMonthDayString(getWeekStartDate());
         String weekEndDate = getMonthDayString(getWeekEndDate());
 
         currentWeekText.setText(weekStartDate + " - " + weekEndDate);
+
+        initializeWeeklyData();
+
+        // Progress bar setup:
+        Drawable draw= getResources().getDrawable(R.drawable.custom_progress_bar);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgressDrawable(draw);
+
+        setupProgressBar();
+
+
     }
 
-    private static Calendar getWeekStartDate() {
+    private Calendar getWeekStartDate() {
         Calendar calendar = Calendar.getInstance(Locale.getDefault());
         while (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
             calendar.add(Calendar.DATE, -1);
@@ -41,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         return calendar;
     }
 
-    private static Calendar getWeekEndDate() {
+    private Calendar getWeekEndDate() {
         Calendar calendar = Calendar.getInstance(Locale.getDefault());
         while (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
             calendar.add(Calendar.DATE, 1);
@@ -50,14 +73,14 @@ public class MainActivity extends AppCompatActivity {
         return calendar;
     }
 
-    private static String getMonthDayString(Calendar calendar) {
+    private String getMonthDayString(Calendar calendar) {
         int monthNumber = calendar.get(Calendar.MONTH);
         int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
         return getMonthName(monthNumber) + " " + dayOfMonth;
     }
 
-    private static String getMonthName(int monthNumber) {
+    private String getMonthName(int monthNumber) {
         switch (monthNumber) {
             case 0: return "Jan";
             case 1: return "Feb";
@@ -75,4 +98,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void initializeWeeklyData() {
+        EditText dailyCal = (EditText) findViewById(R.id.dailyCal);
+        dailyCalorieGoal = Integer.parseInt(dailyCal.getText().toString());
+        weeklyCalorieGoal = 7 * dailyCalorieGoal;
+
+        weeklyCalories[0] = Integer.parseInt(((EditText)findViewById(R.id.mondayCalories)).getText().toString());
+        weeklyCalories[1] = Integer.parseInt(((EditText)findViewById(R.id.tuesdayCalories)).getText().toString());
+        weeklyCalories[2] = Integer.parseInt(((EditText)findViewById(R.id.wednesdayCalories)).getText().toString());
+        weeklyCalories[3] = Integer.parseInt(((EditText)findViewById(R.id.thursdayCalories)).getText().toString());
+        weeklyCalories[4] = Integer.parseInt(((EditText)findViewById(R.id.fridayCalories)).getText().toString());
+        weeklyCalories[5] = Integer.parseInt(((EditText)findViewById(R.id.saturdayCalories)).getText().toString());
+        weeklyCalories[6] = Integer.parseInt(((EditText)findViewById(R.id.sundayCalories)).getText().toString());
+
+        weeklyWeight[0] = Integer.parseInt(((EditText)findViewById(R.id.mondayWeight)).getText().toString());
+        weeklyWeight[1] = Integer.parseInt(((EditText)findViewById(R.id.tuesdayWeight)).getText().toString());
+        weeklyWeight[2] = Integer.parseInt(((EditText)findViewById(R.id.wednesdayWeight)).getText().toString());
+        weeklyWeight[3] = Integer.parseInt(((EditText)findViewById(R.id.thursdayWeight)).getText().toString());
+        weeklyWeight[4] = Integer.parseInt(((EditText)findViewById(R.id.fridayWeight)).getText().toString());
+        weeklyWeight[5] = Integer.parseInt(((EditText)findViewById(R.id.saturdayWeight)).getText().toString());
+        weeklyWeight[6] = Integer.parseInt(((EditText)findViewById(R.id.sundayWeight)).getText().toString());
+    }
+
+    private void setupProgressBar() {
+        TextView progressBarProportion = (TextView) findViewById(R.id.progressBarProportion);
+        int totalCalories = 0;
+        for (int dailyCalories : weeklyCalories) {
+            totalCalories += dailyCalories;
+        }
+        progressBarProportion.setText(totalCalories + " / " +  weeklyCalorieGoal);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(totalCalories);
+        progressBar.setMax(weeklyCalorieGoal);
+    }
 }
